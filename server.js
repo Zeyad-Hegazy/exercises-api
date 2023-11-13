0f560b16df8877bec7a58de1db6fc047d4a024a9
Zeyad Hegazy <zhegazy409@gmail.com>
2023-11-13 14:35:15 +0200

    project setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+const workoutRouter = require("./routes/workouts.js");
+
+app.use((req, res, next) => {
+	console.log(req.path, req.method);
+	next();
+});
+
+app.use("/api/workouts",workoutRouter);
+
+app.listen(PORT, () => {
+	console.log(`server running on port ${PORT}`);
+});
