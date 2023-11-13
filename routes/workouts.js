dab22ead34be448913919e54d5f6519cf8c4d1ee
Zeyad Hegazy <zhegazy409@gmail.com>
2023-11-13 14:42:21 +0200

    setup routes for workouts

--- routes/workouts.js
+++ routes/workouts.js
@@ -2,4 +2,24 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/", (req, res, next) => {
+	res.json({ msg: "GET all workouts" });
+});
+
+router.get("/:id", (req, res, next) => {
+	res.json({ msg: "Get single workout" });
+});
+
+router.post("/", (req, res, next) => {
+	res.json({ msg: "POST a new workout" });
+});
+
+router.delete("/:id", (req, res, next) => {
+	res.json({ msg: "DELETE a workout" });
+});
+
+router.patch("/:id", (req, res, next) => {
+	res.json({ msg: "UPDATE a workout" });
+});
+
 module.exports = router;
